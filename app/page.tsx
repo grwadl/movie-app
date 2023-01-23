1367640599362618ec93502bb4fd97ccd8b7de78
grwadl <youngwwad@gmail.com>
2023-01-23 18:57:01 +0200

    added list component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import Card from "@/components/card/Card";
+import List from "@/components/list/List";
+import { IMovie } from "@/services/api/movie/types";
 import { getTrendingAndPopularMovies } from "./(common)";
 import WelcomeSection from "./(components)/WelcomeSection";
+
+const renderMovies = (movie: IMovie) => (
+  <Card
+    img={movie.poster_path}
+    className="basis-40 w-40 shrink-0 relative"
+    key={movie.id}
+  >
+    <div className="content p-2">
+      <h5 className="card-title mt-4 text-base font-bold whitespace-nowrap text-ellipsis overflow-hidden">
+        {movie.title}
+      </h5>
+    </div>
+  </Card>
+);
 
 export default async function Home() {
   const { popularMovies, trendingMovies } = await getTrendingAndPopularMovies();
@@ -9,17 +25,16 @@ export default async function Home() {
     <div className="padded-section">
       <WelcomeSection />
       <h4 className="text-xl font-semibold my-4">What&apos;s popular</h4>
-      <div className="flex overflow-y-scroll gap-5">
-        {trendingMovies?.map((movie) => (
-          <Card
-            img={movie.poster_path}
-            className="h-80 basis-40 w-40 shrink-0"
-            key={movie.id}
-          >
-            {movie.title}
-          </Card>
-        ))}
-      </div>
+      <List
+        list={trendingMovies}
+        renderFunc={renderMovies}
+        className="flex overflow-x-scroll gap-5"
+      />
+      <List
+        list={popularMovies}
+        renderFunc={renderMovies}
+        className="flex overflow-x-scroll gap-5"
+      />
     </div>
   );
 }
--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -7,14 +7,14 @@ type Props = {
 } & React.HtmlHTMLAttributes<HTMLDivElement>;
 
 function Card({ className, img, ...props }: Props) {
-  const cardClassnames = cls(["relative cursor-pointer", className]);
+  const cardClassnames = cls(["cursor-pointer", className]);
 
   return (
     <div {...props} className={cardClassnames}>
       <Image
         width={300}
         height={300}
-        className="absolute w-full h-full object-cover top-0 left-0 z-[-1]"
+        className="w-full h-70 object-cover top-0 left-0 z-[-1] rounded-md"
         src={img}
         placeholder="blur"
         blurDataURL="https://davidkoepp.com/wp-content/themes/blankslate/images/Movie%20Placeholder.jpg"
